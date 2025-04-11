0033cf00afca5b32406d6c1440d15aa5e6c8b6fa
ADMIN <ADMIN@PRABHA-NANDINI7>
2025-04-11 19:25:04 -0400

    Add project files.

---
+++ oops2/Program.cs
@@ -0,0 +1,35 @@
+﻿
+    namespace oops2
+    {
+        internal class Employee
+        {
+            ulong eno;
+            string ename;
+            double esal;
+            internal void SetEmp()
+            {
+                eno = 123;
+                ename = "Saikumar";
+                esal = 5000;
+            }
+            internal void DisplayEmp()
+            {
+                Console.WriteLine("Emp no is:" + eno);
+                Console.WriteLine("Emp Name is:" + ename);
+                Console.WriteLine("Emp Salary is:" + esal);
+                Console.ReadLine();
+            }
+            class Program
+            {
+                static void Main(string[] args)
+                {
+                    Employee emp1 = new Employee();
+                    emp1.SetEmp();
+                    emp1.DisplayEmp();
+                    Console.ReadLine();
+                }
+
+
+            }
+        }
+    }
